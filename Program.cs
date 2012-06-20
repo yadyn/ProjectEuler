4e5f4bf06162499bbc1f07e2eade573ce85fbc4f
Sean A. Hanley <yadyn@yahoo.com>
2012-06-20 12:34:47 -0500

    Adding initial project structure

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using ProjectEuler.Solutions;
+
+namespace ProjectEuler
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			IPESolver solver = new Problem1();
+
+			solver.Solve();
+		}
+	}
+}
---
+++ Solutions/Problem1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectEuler.Solutions
+{
+	public class Problem1 : IPESolver
+	{
+		public void Solve()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
